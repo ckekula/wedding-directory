54c33c58e16a2a82d95edbd173c047720c325005
Hansika <nostochk@gmail.com>
2024-08-12 11:53:26 +0530

    adjust Head component in the app router

--- frontend/src/components/EditVisitorProfileModal.tsx
+++ frontend/src/components/EditVisitorProfileModal.tsx
@@ -10,7 +10,7 @@ interface EditProfileModalProps {
     lastName: string;
     partnerFirstName: string;
     partnerLastName: string;
-    
+
     engagementDate: Date;
     weddingDate: Date;
     weddingVenue: string;
@@ -55,112 +55,111 @@ const EditProfileModal = ({
         }`}
       ></div>
       <div
-        className={`fixed top-0 right-0 h-full bg-white p-8 rounded-l-2xl w-full max-w-sm shadow-lg transform transition-transform duration-300 ${
+        className={`fixed top-0 right-0 h-full bg-background p-8 rounded-l-2xl w-full max-w-[450px] shadow-lg transform transition-transform duration-300 ${
           isOpen ? "translate-x-0" : "translate-x-full"
         } z-50`}
       >
-        <h2 className="text-2xl font-bold mb-4 font-title text-center">
+        <h2 className="text-2xl font-bold m-auto font-title text-center">
           Edit Profile Settings
         </h2>
         <form className="my-20">
-          <div className="grid grid-cols-2 gap-4 font-body">
+          <div className="grid grid-cols-2 gap-4 font-body ">
             <div className="col-span-2 sm:col-span-1 border-black border-solid">
-             
               <Input
                 name="firstName"
                 type="string"
                 placeholder="First Name"
                 value={formData.firstName}
                 onChange={handleChange}
+                className="text-black font-semibold"
               />
             </div>
             <div className="col-span-2 sm:col-span-1">
-             
               <Input
                 name="lastName"
                 type="string"
                 placeholder="Last Name"
                 value={formData.lastName}
                 onChange={handleChange}
+                className="text-black font-semibold"
               />
             </div>
             <div className="col-span-2 sm:col-span-1">
-              
               <Input
                 name="partnerFirstName"
                 type="string"
-                placeholder="Partner&apos;s First Name"
+                placeholder="Partner's First Name"
                 value={formData.partnerFirstName}
                 onChange={handleChange}
-                
+                className="text-black font-semibold"
               />
             </div>
             <div className="col-span-2 sm:col-span-1">
-              
               <Input
                 name="partnerLastName"
-                placeholder="Partner&apos;s Last Name"
+                placeholder="Partner's Last Name"
                 type="string"
                 value={formData.partnerLastName}
                 onChange={handleChange}
+                className="text-black font-semibold"
               />
             </div>
             <div className="col-span-2">
-             
               <Input
                 name="engagementDate"
                 type="date"
                 placeholder="Engagement Date"
                 value={formData.engagementDate.toISOString().split("T")[0]}
                 onChange={handleChange}
+                className="text-black font-semibold"
               />
             </div>
             <div className="col-span-2">
-              
               <Input
                 name="weddingDate"
                 placeholder="Wedding Date"
                 type="date"
                 value={formData.weddingDate.toISOString().split("T")[0]}
                 onChange={handleChange}
+                className="text-black font-semibold"
               />
             </div>
             <div className="col-span-2">
-              
               <Input
                 name="weddingVenue"
                 type="string"
                 placeholder="Wedding Venue"
                 value={formData.weddingVenue}
                 onChange={handleChange}
+                className="text-black font-semibold"
               />
             </div>
             <div className="col-span-2">
-              
               <Input
                 name="email"
                 placeholder="Email"
                 value={formData.email}
                 onChange={handleChange}
+                className="text-black font-semibold"
               />
             </div>
             <div className="col-span-2">
-              
               <Input
                 type="password"
                 name="password"
                 placeholder="Password"
                 value={formData.password}
                 onChange={handleChange}
+                className="text-black font-semibold"
               />
             </div>
           </div>
         </form>
-        <div className="flex justify-end mt-4">
-          <Button variant="secondary" onClick={onClose} className="mr-2">
+        <div className=" flex justify-between mt-4 gap-2">
+          <Button  onClick={onClose} className="m-auto w-[210px] bg-accent font-body">
             Cancel
           </Button>
-          <Button onClick={handleSave}>Save</Button>
+          <Button onClick={handleSave}  className="m-auto w-[210px] bg-accent font-body" >Save</Button>
         </div>
       </div>
     </>
