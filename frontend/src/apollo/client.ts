ef2a96c1c8bdc46e011954dd4682148eb88e8ece
chamuditha123 <chamukeku@gmail.com>
2024-08-14 17:54:12 +0530

    Setup Apollo Client in frontend

---
+++ frontend/src/apollo/client.ts
@@ -0,0 +1,23 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql', // replace with your backend URL
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default client;
