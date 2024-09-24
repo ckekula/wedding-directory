24b633467144d8529b7d636ad557cc0a48ac28a4
Hansika <nostochk@gmail.com>
2024-09-24 09:21:20 +0530

    add accordion component from shadcn ui

--- frontend/src/app/visitor-dashboard/page.tsx
+++ frontend/src/app/visitor-dashboard/page.tsx
@@ -17,14 +17,14 @@ const VisitorDashboard = () => {
           </div>
           <div className="w-4/5 py-10">
             <div className="mr-48">
-              <div className="flex justify-around">
+              <div className="flex justify-evenly">
                 <div className="flex ">
                   <div className="rounded-xl overflow-hidden transform -rotate-12 shadow-lg">
-                     <Image
+                    <Image
                       src={profilePic}
                       alt="dashboard profile picture"
                       className="w-[175px] h-[175px]"
-                    /> 
+                    />
                   </div>
                   <div className=" font-merriweather">
                     <div className="flex flex-col text-center">
@@ -45,23 +45,24 @@ const VisitorDashboard = () => {
                   <p className="font-body text-sm mt-4">Set Budget</p>
                 </div>
               </div>
-              <div className="flex ">
+              <div className="flex mt-4 justify-center gap-8 font-body">
                 <div>Add Date</div>
                 <div>Add venue</div>
-                <div>Add number of guests</div>
+                <div>No of guests</div>
               </div>
+              <hr className=" w-3/5 h-1 mx-auto my-4 bg-slate-400 border-0 rounded md:my-6 "></hr>
 
-              <div className="flex">
+              <div className="flex justify-around">
                 <div>Venodr block</div>
                 <div>Venue block</div>
               </div>
 
-              <div className="flex">
+              <div className="flex justify-around">
                 <div>guest list block</div>
                 <div>anouncement block</div>
               </div>
 
-              <div>Wedding planning guide block</div>
+              <div className="flex justify-center">Wedding planning guide block</div>
             </div>
           </div>
         </div>
