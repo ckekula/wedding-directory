76aabbcf0e94a2d599d05d587094c9c759fa81db
Keshara Gunathilaka <gpkhgunathilaka@gmail.com>
2024-09-26 03:24:53 +0530

    pageone 2.0
    fix structure

--- frontend/src/app/(onboarding)/pageone/page.tsx
+++ frontend/src/app/(onboarding)/pageone/page.tsx
@@ -2,86 +2,112 @@ import React from 'react'
 import Image from "next/image";
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import { X } from 'lucide-react';
+import { FaCircleCheck } from "react-icons/fa6";
+import { TbCircleNumber1, TbCircleNumber1Filled, TbCircleNumber2, TbCircleNumber3 } from "react-icons/tb";
+import { GoHorizontalRule } from "react-icons/go";
+
 
 const page = () => {
     return (
-        <div className="bg-background">
+        <div className="bg-background font-title">
             <div className="flex flex-col md:flex-row h-screen">
 
-                    <div className="w-full m-5 md:w-1/2 h-1/2 md:h-full relative rounded-2xl overflow-hidden">
- 
+                <div className="relative w-full m-3 md:w-5/12 h-full md:h-auto rounded-2xl overflow-hidden">
                     <Image
-                        src="/bride.jpg"
+                        src="/onboard2.jpg"
                         layout="fill"
                         objectFit="cover"
-                        alt="sign image"
-                        
+                        alt="onboard image"
                     />
-                    
                 </div>
 
+                <button
+                    className="absolute top-5 right-5 text-black hover:text-gray-500" >
+                    <X className="w-6 h-6" />
+                </button>
 
+                <div className="w-full md:w-7/12 p-5 border-l-2 border-black">
+                    <button
+                        className="absolute top-5 transform -translate-x-1/6 text-black font-body hover:text-gray-500">  &larr; Back
+                    </button>
 
-                <div className="w-full md:w-1/2 p-10 flex flex-col border-l-2 border-black">
-                    <button className="mb-4 text-left text-gray-500 text-sm">&larr; Back</button>
+                    <div className='flex flex-col justify-center items-center p-10 mt-8'>
 
-                    <div className=" p-10 justify-center">
+                        <div className="flex items-center font-body justify-center text-center space-x-2 text-lg mt-8 mb-3">
+                            {/* <FaCircleCheck /> */}
+                            <TbCircleNumber1Filled />
+                            <label className='text-base'>Get Started</label>
+                            <GoHorizontalRule className='text-6xl' />
+                            <TbCircleNumber2 />
+                            <label className='text-base'>Plan</label>
+                            <GoHorizontalRule className='text-6xl' />
+                            <TbCircleNumber3 />
+                            <label className='text-base'>Finish Up</label>
+                        </div>
 
-                        <h2 className="text-2xl font-semibold mb-4">Like any great relationship, this one starts with basics</h2>
+                        <div>
+                            <h2 className="text-2xl text-center mb-6 ">Like any great relationship, this one starts with basics</h2>
+                        </div>
+                        
 
-                        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mb-6">
+                        <div className="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 mb-6">
                             <div className="w-full md:w-1/2 h-1/2 md:h-full relative">
-                                <label className="block mb-3">First Name</label>
-                                <div className="border-black border-solid border-2 rounded-lg w-2/3">
-                                    <Input className="h-8" type="text" id="" placeholder="" />
+                                <label className="block font-light mb-2">First name</label>
+                                <div className="border-gray-300 border-solid rounded-xl border-2 w-10/12">
+                                    <Input className="h-9 w-full rounded-xl" type="text" id="" placeholder="" />
                                 </div>
                             </div>
                             <div className="w-full md:w-1/2 h-1/2 md:h-full relative">
-                                <label className="block mb-3">First Name</label>
-                                <div className="border-black border-solid border-2 rounded-lg w-2/3">
-                                    <Input className="h-8" type="text" id="" placeholder="" />
+                                <label className="block font-light mb-2">Last name</label>
+                                <div className="border-gray-300 border-solid rounded-xl border-2 w-10/12">
+                                    <Input className="h-9 w-full rounded-xl" type="text" id="" placeholder="" />
                                 </div>
                             </div>
                         </div>
-                        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mb-6">
+                        <div className="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 mb-6">
                             <div className="w-full md:w-1/2 h-1/2 md:h-full relative">
-                                <label className="block mb-3">First Name</label>
-                                <div className="border-black border-solid border-2 rounded-lg w-2/3">
-                                    <Input className="h-8" type="text" id="" placeholder="" />
+                                <label className="block font-light mb-2">Partner&apos;s first Name</label>
+                                <div className="border-gray-300 border-solid rounded-xl border-2 w-10/12">
+                                    <Input className="h-9 w-full rounded-xl" type="text" id="" placeholder="" />
                                 </div>
                             </div>
                             <div className="w-full md:w-1/2 h-1/2 md:h-full relative">
-                                <label className="block mb-3">First Name</label>
-                                <div className="border-black border-solid border-2 rounded-lg w-2/3">
-                                    <Input className="h-8" type="text" id="" placeholder="" />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mb-6">
-                            <div className="w-full md:w-1/2 h-1/2 md:h-full relative">
-                                <label className="block mb-3">First Name</label>
-                                <div className="border-black border-solid border-2 rounded-lg w-2/3">
-                                    <Input className="h-8" type="date" id="" placeholder="" />
-                                </div>
-                            </div>
-                            <div className="w-full md:w-1/2 h-1/2 md:h-full relative">
-                                <label className="block mb-3">First Name</label>
-                                <div className="border-black border-solid border-2 rounded-lg w-2/3">
-
+                                <label className="block font-light mb-2">Partner&apos;s last name</label>
+                                <div className="border-gray-300 border-solid rounded-xl border-2 w-10/12">
+                                    <Input className="h-9 w-full rounded-xl" type="text" id="" placeholder="" />
                                 </div>
                             </div>
                         </div>
+                        <div className="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 mb-6">
+                            <div className="w-full md:w-1/2 h-1/2 md:h-full relative">
+                            <div className="flex items-center">
+                                <label className="block font-light w-fit mb-2 mr-1">Wedding date</label>
+                                <span className="text-gray-400 text-sm">(Don&apos;t worry! You can change this later)</span>
+                                </div>
+                                <div className="border-gray-300 border-solid rounded-xl border-2 w-10/12">
+                                    <Input className="h-9 w-full rounded-xl" type="date" id="" placeholder="" />
+                                </div>
+                            </div>
+                            <div className="w-full md:w-1/3 h-1/2 md:h-full relative flex items-center">
+                                <Checkbox className="mr-2 mt-10" />
+                                <label className="block font-light mt-10">We&apos;re still deciding</label>
+                            </div>
+                        </div>
 
-                        <div className="  mt-6 flex flex-col w-full ">
+                        <div className="mt-4 flex flex-col w-7/12 ">
                             <Button className="rounded-none text-black font-bold hover:bg-primary bg-primary text-lg">
-                                Log In
+                               Next
                             </Button>
-                        </div>
-                    </div>
-                    <button className="mt-20 text-center text-gray-500 text-sm"> Back</button>
+                            </div>
+                    <button className="mt-12 text-center text-black "> Skip the onboarding process</button>
+                
+                </div>
+                </div>
                 </div>
             </div>
-        </div>
+       
     )
 }
 
