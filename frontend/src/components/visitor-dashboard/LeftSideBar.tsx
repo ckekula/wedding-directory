bd9d7e0429366063f39cd5ce120dc4c3799e8898
Hansika <nostochk@gmail.com>
2024-09-26 09:33:22 +0530

    add state to manage the active links

--- frontend/src/components/visitor-dashboard/LeftSideBar.tsx
+++ frontend/src/components/visitor-dashboard/LeftSideBar.tsx
@@ -14,21 +14,23 @@ const LeftSideBar: React.FC<LeftSideBarProps> = ({
   isCollapsed,
   setIsCollapsed,
 }) => {
+  const [activeLink, setActiveLink] = useState<string>(""); // State to track the active link
+
   const toggleSideBar = () => {
     setIsCollapsed(!isCollapsed);
   };
 
   return (
     <div
-      className={`flex justify-center font-body  bg-white rounded-br-3xl rounded-tr-3xl shadow-md p-5 transition-all duration-300 ${
+      className={`flex justify-center font-body bg-white rounded-br-3xl rounded-tr-3xl shadow-md p-5 transition-all duration-300 ${
         isCollapsed
-          ? "w-[55px] h-[132px] border-slate-800 border-solid border-2 "
+          ? "w-[55px] h-[132px] border-slate-800 border-solid border-2"
           : "w-64 h-[400px]"
       }`}
     >
       {/* Collapsed State */}
       {isCollapsed ? (
-        <div className="flex  justify-between items-stretch  ">
+        <div className="flex justify-between items-stretch">
           {/* Toggle Button */}
           <button onClick={toggleSideBar} className="focus:outline-none">
             <FaChevronRight />
@@ -40,41 +42,63 @@ const LeftSideBar: React.FC<LeftSideBarProps> = ({
           {/* Navigation Items */}
           <nav className="mt-10">
             <div className="space-y-8">
+              {/* Dashboard Link */}
               <Link
                 href="/visitor-dashboard"
-                className="flex items-center text-black transition-colors duration-300 active:text-orange "
+                onClick={() => setActiveLink("/visitor-dashboard")} // Set active link
+                className={`flex items-center transition-colors duration-300 ${
+                  activeLink === "/visitor-dashboard"
+                    ? "text-orange"
+                    : "text-black"
+                }`}
               >
                 <FiHome className="text-2xl w-[30px] h-[30px]" />
                 <span className="ml-3">Dashboard</span>
               </Link>
 
+              {/* Checklist Link */}
               <Link
                 href="/checklist"
-                className="flex items-center text-black transition-colors duration-300"
+                onClick={() => setActiveLink("/checklist")} // Set active link
+                className={`flex items-center transition-colors duration-300 ${
+                  activeLink === "/checklist" ? "text-orange" : "text-black"
+                }`}
               >
                 <IoMdCheckmarkCircleOutline className="text-2xl w-[30px] h-[30px]" />
                 <span className="ml-3">Checklist</span>
               </Link>
 
+              {/* Budgeter Link */}
               <Link
                 href="/budgeter"
-                className="flex items-center text-black transition-colors duration-300"
+                onClick={() => setActiveLink("/budgeter")} // Set active link
+                className={`flex items-center transition-colors duration-300 ${
+                  activeLink === "/budgeter" ? "text-orange" : "text-black"
+                }`}
               >
                 <FiDollarSign className="text-2xl w-[30px] h-[30px]" />
                 <span className="ml-3">Budgeter</span>
               </Link>
 
+              {/* Guest List Link */}
               <Link
                 href="/guest-list"
-                className="flex items-center text-black transition-colors duration-300"
+                onClick={() => setActiveLink("/guest-list")} // Set active link
+                className={`flex items-center transition-colors duration-300 ${
+                  activeLink === "/guest-list" ? "text-orange" : "text-black"
+                }`}
               >
                 <FiUsers className="text-2xl w-[30px] h-[30px]" />
                 <span className="ml-3">Guest List</span>
               </Link>
 
+              {/* Vendors Link */}
               <Link
                 href="/vendors"
-                className="flex items-center text-black transition-colors duration-300"
+                onClick={() => setActiveLink("/vendors")} // Set active link
+                className={`flex items-center transition-colors duration-300 ${
+                  activeLink === "/vendors" ? "text-orange" : "text-black"
+                }`}
               >
                 <HiOutlineBriefcase className="text-2xl w-[30px] h-[30px]" />
                 <span className="ml-3">Vendors</span>
