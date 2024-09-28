a2bef9d91b26c08cda8495dc28e900a9354b5ca8
Cooper-JSK <janithakarunarathna12@gmail.com>
2024-09-28 21:49:28 +0530

    updated repo

--- backend/src/modules/vendor/vendor.service.ts
+++ backend/src/modules/vendor/vendor.service.ts
@@ -4,6 +4,7 @@ import { VendorFilterInput } from 'src/graphql/inputs/vendorFilter';
 import { DataSource } from 'typeorm';
 import { VendorRepositoryType, VendorRepository } from 'src/database/repositories/vendor.repository';
 import { CreateVendorInput } from 'src/graphql/inputs/createVendor';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class VendorService {
@@ -26,8 +27,8 @@ export class VendorService {
   }
 
   async createVendor(createVendorInput: CreateVendorInput): Promise<VendorEntity> {
-    const vendor = this.vendorRepository.create(createVendorInput);
+    const hashedPassword = await bcrypt.hash(createVendorInput.password, 12);
+    const vendor = this.vendorRepository.create({ ...createVendorInput, password: hashedPassword });
     return this.vendorRepository.save(vendor);
   }
-
 }
