a2bf15525e74c59d3443867044efcdf365f071b2
Hansika <nostochk@gmail.com>
2024-10-02 02:46:03 +0530

    start creating dashboard landing page

--- frontend/src/app/vendor-dashboard/page.tsx
+++ frontend/src/app/vendor-dashboard/page.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Header from "@/components/shared/Headers/Header";
+import VendorBanner from "@/components/vendor-dashboard/VendorBanner";
 
 const EditProfilePage = () => {
-  return <Header />;
+  return (
+    <Fragment>
+      <Header />
+      <div className="bg-lightYellow">
+        <div className="container ">
+          <h2 className="font-title font-bold text-[36px] p-8 text-center">
+            Welcome
+          </h2>
+          <VendorBanner businessName="John's Flower Shop" />
+          <div className="flex justify-center gap-10 my-8 font-body ">
+            <p>Category</p>
+            <p>Member since</p>
+            <p>Rating</p>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default EditProfilePage;
