52fd23403103a0f1cce6220e5ca00a40b1090055
Hansika <nostochk@gmail.com>
2024-10-02 01:48:00 +0530

    finish vendor dashboard/settings page

--- frontend/src/app/vendor-dashboard/page.tsx
+++ frontend/src/app/vendor-dashboard/page.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import Header from "@/components/shared/Headers/Header";
 import VendorBanner from "@/components/vendor-dashboard/VendorBanner";
+import Settings from "@/components/vendor-dashboard/dashboard-settings/Settings";
+import EditProfile from "@/components/vendor-dashboard/dashboard-settings/EditProfile";
 
-const page = () => {
+const EditProfilePage = () => {
   return (
     <div className="">
       <Header />
-      <div className="bg-lightYellow h-screen">
+      <div className="bg-lightYellow min-h-screen">
         <div className="p-20">
+          {/* Vendor Banner */}
           <VendorBanner businessName="John's Flower Shop" />
+        </div>
+
+        <div className="container mx-auto flex space-x-10 ">
+          {/* Sidebar (Settings) */}
+          <div className="w-1/4">
+            <Settings />
+          </div>
+
+          {/* Edit Profile Section */}
+          <div className="w-3/4  ">
+            <EditProfile />
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default page;
+export default EditProfilePage;
--- frontend/src/components/vendor-dashboard/VendorBanner.tsx
+++ frontend/src/components/vendor-dashboard/VendorBanner.tsx
@@ -20,7 +20,11 @@ const VendorBanner: React.FC<VendorBannerProps> = ({ businessName }) => {
 
       {/* Business name div */}
       <div className="absolute bottom-4 left-4 bg-white text-black py-2 px-4 rounded-xl flex items-center">
-        <FiHome className="mr-2 font-bold" size={20} style={{ strokeWidth: 2.5 }}/>
+        <FiHome
+          className="mr-2 font-bold"
+          size={20}
+          style={{ strokeWidth: 2.5 }}
+        />
         <p className="font-bold font-title text-[20px]">{businessName}</p>
       </div>
     </div>
