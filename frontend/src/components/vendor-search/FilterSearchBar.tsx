f1badf888ca6f99e95aae5545f8927b591042dd2
nostoc <nostochk@gmail.com>
2024-10-11 23:09:14 +0530

    refactor searchbar component

--- frontend/src/components/vendor-search/FilterSearchBar.tsx
+++ frontend/src/components/vendor-search/FilterSearchBar.tsx
@@ -1,8 +1,9 @@
-"use-client"
-import React, { useState } from 'react';
+"use-client";
+import React, { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
-import CategoryInput from './CategoryInput';
-import CityInput from './CityInput';
+import CategoryInput from "./CategoryInput";
+import CityInput from "./CityInput";
+import { Button } from "../ui/button";
 const SearchBar: React.FC = () => {
   const [category, setCategory] = useState<string | null>(null);
   const [city, setCity] = useState<string | null>(null);
@@ -21,29 +22,29 @@ const SearchBar: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center py-10">
-      <div className="flex items-center bg-white shadow-lg rounded-full w-[650px] h-[55px] px-4 space-x-4">
-        
+    <div className="flex items-center justify-center py-10 ">
+      <div className="flex items-center bg-white shadow-lg rounded-full w-[650px] h-[70px] px-4 space-x-4">
         {/* Category Input */}
         <div className="relative flex-1">
-          <label className="block text-xs font-semibold mb-1">Category</label>
+          <label className="block text-xs text-slate-600 mb-1">Category</label>
           <CategoryInput onCategoryChange={handleCategoryChange} />
         </div>
         {/* Location Input */}
         <div className="relative flex-1">
-          <label className="block text-xs font-semibold mb-1">Location</label>
+          <label className="block text-xs text-slate-600 mb-1">Location</label>
           <CityInput onCityChange={handleCityChange} />
         </div>
 
-        
-
         {/* Search Button */}
-        <button
-          onClick={handleSearch}
-          className="flex items-center justify-center bg-orange text-white text-xl rounded-full p-3 hover:bg-[#e91c5d] transition"
-        >
-          <IoIosSearch />
-        </button>
+        <div className="flex justify-end rounded-s-none">
+          <Button
+            onClick={handleSearch}
+            className="flex items-center justify- h-full rounded-r-full  w-[70px] mr-0 "
+            variant="signup"
+          >
+            <IoIosSearch size={24} />
+          </Button>
+        </div>
       </div>
     </div>
   );
