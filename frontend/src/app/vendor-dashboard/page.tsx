6ee49c6129b13ab09f79395c71fe325177a55bef
KesharaGunathilaka <gpkhgunathilaka@gmail.com>
2024-10-12 00:54:50 +0530

    vendor-dashboard
    update vendor banner and add services

--- frontend/src/app/vendor-dashboard/page.tsx
+++ frontend/src/app/vendor-dashboard/page.tsx
@@ -4,43 +4,41 @@ import VendorBanner from "@/components/vendor-dashboard/VendorBanner";
 import Stats from "@/components/vendor-dashboard/Stats";
 import QuickActions from "@/components/vendor-dashboard/QuickActions";
 import ToDo from "@/components/vendor-dashboard/ToDo";
+import ProfileInfo from "@/components/vendor-dashboard/ProfileInfo";
+import VendorResult from "@/components/vendor-search/VendorResult";
 
 const VendorDashBoard = () => {
   return (
     <div className="">
       <Header />
       <div className="bg-lightYellow min-h-screen">
-        <div className="container mx-auto px-4 "> 
-          <h1 className="font-title text-[36px] text-black text-center my-10 pt-10">Welcome</h1>
-          {/* Vendor Banner */}
-          <VendorBanner businessName="John's Flower Shop" />
-
-          {/* Category, Member Since, Rating */}
-          <div className="flex justify-evenly items-center gap-10 mt-10 mb-8">
-            <div className="flex flex-col justify-center items-center">
-              <p className=" font-body text-[20px]">Category</p>
-              <p className=" font-body text-[15px]">Florist</p>
-            </div>
-            <div className="flex flex-col justify-center items-center">
-              <p className=" font-body text-[20px]">Member Since</p>
-              <p className=" font-body text-[15px]">2024</p>
-            </div>
-            <div className="flex flex-col justify-center items-center">
-              <p className=" font-body text-[20px]">Rating</p>
-              <p className=" font-body text-[15px]">4.9</p>
-            </div>
-          </div>
-
-          {/* Stats */}
-          <div className="flex justify-center">
-            <Stats />
-          </div>
+        <div className="container mx-auto px-4 pt-10 ">
+          <ProfileInfo />
 
           {/* Quick Actions and To Do's */}
-          <div className="flex justify-between py-12 gap-10"> {/* Adjusted spacing and alignment */}
-            <QuickActions  />  {/* Giving each section half the width */}
-            <ToDo  />  {/* Ensuring equal width for better alignment */}
+          <div className="flex justify-between py-12 gap-10">
+            {" "}
+            {/* Adjusted spacing and alignment */}
+            <QuickActions /> {/* Giving each section half the width */}
+            <ToDo /> {/* Ensuring equal width for better alignment */}
           </div>
+          <hr className="border-t border-gray-300 my-4" />
+          <div className="flex flex-row">
+          <div className="w-5/6 text-2xl font-bold mb-8">Your Services</div>
+          <div className="w-1/6 ml-10">Add new Service</div></div>
+          <div className="grid grid-cols-3 gap-6 overflow-x-auto">
+            <VendorResult
+              key="sdf"
+              vendor="sfsfd"
+              city={"sdsy"}
+              banner={"/login-signup.jpg"}
+              rating="⭐ 4.9 (154)" // customize the rating
+              price="$$-$$$" // customize the price
+              about={"No description available"}
+            />            
+          </div>
+          <hr className="border-t border-gray-300 my-4" />
+          <div className="text-2xl font-bold mb-8">Messages and Inquiries</div>
         </div>
       </div>
     </div>
