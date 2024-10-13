065b3c83d3cf97197a1ecbf15c67975d2669db8f
nostoc <nostochk@gmail.com>
2024-10-13 09:07:10 +0530

    connect terms of use and privacy policy pages to footer

--- frontend/src/components/shared/Footer.tsx
+++ frontend/src/components/shared/Footer.tsx
@@ -60,12 +60,12 @@ const Footer = () => {
                   </Link>
                 </li>
                 <li>
-                  <Link href="/privacy" className="hover:text-orange">
+                  <Link href="/privacy-policy" className="hover:text-orange">
                     Privacy Policy
                   </Link>
                 </li>
                 <li>
-                  <Link href="/terms" className="hover:text-orange">
+                  <Link href="/terms-of-use" className="hover:text-orange">
                     Terms and Conditions
                   </Link>
                 </li>
