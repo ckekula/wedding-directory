8a75bedc603458b3382253f06eed165ef828f6ff
nostoc <nostochk@gmail.com>
2024-10-17 11:18:14 +0530

    update font configuration

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,16 +1,40 @@
 import type { Metadata } from "next";
 import { AuthProvider as VisitorAuthProvider } from "@/contexts/VisitorAuthContext";
 import ApolloWrapper from "@/apollo/ApolloWrapper";
-import { montserrat, merriweather, montez } from "@/lib/fonts";
+import { Montserrat, Merriweather, Montez, Outfit } from "next/font/google";
 import "./globals.css";
 import PageTransition from "@/components/PageTransition";
-import { VendorAuthProvider } from '@/contexts/VendorAuthContext';
-import { Toaster } from 'react-hot-toast';
+import { VendorAuthProvider } from "@/contexts/VendorAuthContext";
+import { Toaster } from "react-hot-toast";
 
 export const metadata: Metadata = {
   title: "Say I Do",
   description: "Crafting Timeless Celebrations ",
 };
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-montserrat",
+});
+
+const merriweather = Merriweather({
+  subsets: ["latin"],
+  weight: ["300", "400", "700", "900"],
+  variable: "--font-merriweather",
+});
+
+const montez = Montez({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-montez",
+});
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["100","400","700"],
+  variable: "--font-outfit",
+});
 
 export default function RootLayout({
   children,
@@ -19,27 +43,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-   
-        <link rel="icon" href="/favicon.ico" />
-      
+      <link rel="icon" href="/favicon.ico" />
+
       <body
-        className={`${montserrat.variable} ${merriweather.variable} ${montez.variable}`}
+        className={`${montserrat.variable} ${merriweather.variable} ${montez.variable} ${outfit.variable}`}
       >
-      <ApolloWrapper>
-        {/* Wrapping the application with VisitorProvider */}
-        <VisitorAuthProvider>
-          <VendorAuthProvider>
-            <PageTransition>
-              {children}
-              <Toaster reverseOrder={false} />
-            </PageTransition>
-
-          </VendorAuthProvider>
-          {/* PageTransition can wrap around the children to handle animations */}
-
-        </VisitorAuthProvider>
-      </ApolloWrapper>
-        
+        <ApolloWrapper>
+          {/* Wrapping the application with VisitorProvider */}
+          <VisitorAuthProvider>
+            <VendorAuthProvider>
+              <PageTransition>
+                {children}
+                <Toaster reverseOrder={false} />
+              </PageTransition>
+            </VendorAuthProvider>
+            {/* PageTransition can wrap around the children to handle animations */}
+          </VisitorAuthProvider>
+        </ApolloWrapper>
       </body>
     </html>
   );
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,67 +1,67 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
-  	container: {
-  		center: true,
-  		padding: '35px',
-  		screens: {
-  			sm: '640px',
-  			md: '768px',
-  			lg: '960px',
-  			xl: '1200px'
-  		}
-  	},
-  	extend: {
-  		colors: {
-  			primary: '#F1B4B4',
-  			secondary: '#FFDCDC',
-  			accent: '#1F417F',
-  			background: '#E2E7EF',
-  			text: '#132043',
-  			brown: '#271300',
-  			orange: '#FC7B54',
-  			lightYellow: '#FFF8F3'
-  		},
-  		fontFamily: {
-  			body: ["montserrat", "serif"],
-  			merriweather: ["merriweather", "serif"],
-  			montez: ["montez", "cursive"],
-  			title: ["outfit", "sans-serif"]
-  		},
-  		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			},
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
-  		}
-  	}
+    container: {
+      center: true,
+      padding: "35px",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "960px",
+        xl: "1200px",
+      },
+    },
+    extend: {
+      colors: {
+        primary: "#F1B4B4",
+        secondary: "#FFDCDC",
+        accent: "#1F417F",
+        background: "#E2E7EF",
+        text: "#132043",
+        brown: "#271300",
+        orange: "#FC7B54",
+        lightYellow: "#FFF8F3",
+      },
+      fontFamily: {
+        body: ["var(--font-montserrat)", "sans-serif"],
+        merriweather: ["var(--font-merriweather)", "serif"],
+        montez: ["var(--font-montez)", "cursive"],
+        title: ["var(--font-outfit)", "cursive"],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
