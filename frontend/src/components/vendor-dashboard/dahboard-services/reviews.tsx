88165ed9a1a42b5c6032522eb960c17f4cd2055f
KesharaGunathilaka <gpkhgunathilaka@gmail.com>
2024-11-08 00:43:37 +0530

    review stars
    add review stars

--- frontend/src/components/vendor-dashboard/dahboard-services/reviews.tsx
+++ frontend/src/components/vendor-dashboard/dahboard-services/reviews.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Progress } from "@/components/ui/progress"
+import { FaRegStar } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+
 
 
 const Reviews = () => {
     return (
         <div>
-            <div className='flex w-full'>
-                <div className='w-1/2 flex flex-col'>
-                    sdsd
-
+            <div className='flex w-full font-body'>
+                <div className='w-1/2 flex flex-row text-3xl text-yellow-400 mt-5 ml-3'>
+                <FaStar/>
+                <FaStar/>
+                <FaStar/>
+                <FaStar/>
+                <FaRegStar/>
+                <div className='text-black text-2xl ml-3'>4 (30)</div>
                 </div>
+
                 <div className='w-1/2 flex flex-col'>
                     <div className='flex flex-row items-center'>
                         <p className='mr-2 mb-2 w-20'>5 Stars</p>
