fb9d788fbe181216beccbc3fb99072164714a84b
Janitha Senani Karunarathna <janithakarunarathna12@gmail.com>
2024-11-11 10:54:29 +0530

    Dock for mobile is still underway

--- frontend/src/components/visitor-dashboard/WeddingCoupleCard.tsx
+++ frontend/src/components/visitor-dashboard/WeddingCoupleCard.tsx
@@ -15,33 +15,36 @@ const WeddingCoupleCard: React.FC<WeddingCoupleProps> = ({ profilePic, setProfil
   const daysLeft = calculateDaysLeft();
 
   return (
-    <div className="w-full max-w-8xl mx-auto bg-white shadow-lg rounded-lg p-8">
-      <div className="flex flex-col md:flex-row items-center gap-8">
+    <div className="w-full max-w-8xl mx-auto bg-white shadow-lg rounded-lg p-4">
+      <div className="flex flex-col md:flex-row items-center gap-4">
+
+        {/* Text Container - Center column */}
+        <div className="flex-1 flex items-center justify-center text-center">
+          <div>
+            <p className="text-gray-800 mb-4 text-sm">The marriage of</p>
+            <div className="space-y-0">
+              <div className="text-4xl font-marck -ml-20">{brideName || 'Bride'}</div>
+              <div className="text-lg ml-2">AND</div>
+              <div className="text-4xl font-marck -mr-24">{groomName || 'Groom'}</div>
+            </div>
+          </div>
+        </div>
+        {/* Countdown Container - Right side on desktop */}
+        <div className="w-full md:w-1/3 flex flex-col items-center justify-center text-center rounded-lg p-4">
+          <p className="text-gray-800 mb-2">Days until the wedding:</p>
+          <div className="text-3xl font-bold text-orange">
+            {daysLeft} {daysLeft === 1 ? 'day' : 'days'}
+          </div>
+        </div>
         {/* Profile Picture Upload - Left side on desktop */}
         <div className="w-full md:w-1/3 flex justify-center">
           {/* Integrating ProfilePicture component here */}
           <ProfilePicture profilePic={profilePic} setProfilePic={setProfilePic} />
         </div>
 
-        {/* Text Container - Center column */}
-        <div className="flex-1 flex items-center justify-center text-center">
-          <div>
-            <p className="text-gray-800 mb-4 text-2xl">The marriage of</p>
-            <div className="space-y-0">
-              <div className="text-5xl font-marck -ml-20">{brideName || 'Bride'}</div>
-              <div className="text-lg ml-2">AND</div>
-              <div className="text-5xl font-marck -mr-24">{groomName || 'Groom'}</div>
-            </div>
-          </div>
-        </div>
 
-        {/* Countdown Container - Right side on desktop */}
-        <div className="w-full md:w-1/3 flex flex-col items-center justify-center text-center rounded-lg p-4">
-          <p className="text-gray-800 mb-2">Days until the wedding:</p>
-          <div className="text-3xl font-bold text-green-500">
-            {daysLeft} {daysLeft === 1 ? 'day' : 'days'}
-          </div>
-        </div>
+
+
       </div>
     </div>
   );
