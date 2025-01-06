1a9dcb38adb01686ca7293791281abec238281fa
ckekula <chamukeku@gmail.com>
2025-01-06 22:03:59 +0530

    update service page

--- frontend/src/components/vendor-dashboard/dahboard-services/socialIcons.tsx
+++ frontend/src/components/vendor-dashboard/dahboard-services/socialIcons.tsx
@@ -2,41 +2,51 @@ import React from 'react'
 import { SlSocialFacebook, SlSocialInstagram } from "react-icons/sl";
 import { GoGlobe } from "react-icons/go";
 import { FaXTwitter } from "react-icons/fa6";
+import { SocialTypes } from '@/types/offeringTypes';
 
-type SocialService = {
-    website?: string;
-    facebook?: string;
-    instagram?: string;
-    x?: string;
-  };
-
-const SocialIcons = ({service}: { service: SocialService }) => {
+const SocialIcons = ({offering}: { offering: SocialTypes }) => {
   return (
     
         <div className=' flex flex-row text-2xl items-center justify-end gap-x-4 container'>
             <div
-                onClick={() => window.open(service?.website, '_blank')}
+                onClick={() => {
+                    if(offering?.website) {
+                        window.open(offering.website, '_blank')
+                    }
+                }}
                 className='cursor-pointer'
                 title="Visit Website"
             >
                 <GoGlobe className=" hover:text-orange"/>
             </div>
             <div
-                onClick={() => window.open(service?.facebook, '_blank')}
+                onClick={() => {
+                    if (offering?.facebook) {
+                        window.open(offering.facebook, '_blank');
+                    }
+                }}
                 className='cursor-pointer'
                 title="Visit Facebook Page"
             >
                 <SlSocialFacebook className=" hover:text-orange"/>
             </div>
             <div
-                onClick={() => window.open(service?.instagram, '_blank')}
+                onClick={() => {
+                    if (offering?.instagram) {
+                        window.open(offering.instagram, '_blank');
+                    }}
+                }
                 className='cursor-pointer'
                 title="Visit Instagram Page"
             >
                 <SlSocialInstagram className=" hover:text-orange"/>
             </div>
             <div
-                onClick={() => window.open(service?.x, '_blank')}
+                onClick={() => {
+                    if (offering?.x) {
+                        window.open(offering.x, '_blank');
+                    }}
+                }
                 className='cursor-pointer'
                 title="Visit X Page"
             >
