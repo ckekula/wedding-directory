df14ab64ba887c9728b1764019e9bb3ecdf9c19d
nostoc <nostochk@gmail.com>
2025-05-07 20:26:55 +0530

    change category from  text imput to dropdown in budget item

---
+++ frontend/src/utils/budgetCategories.ts
@@ -0,0 +1,19 @@
+const budgetCategories = [
+  "Venues",
+  "Suits and Dresses",
+  "Catering",
+  "Photographers",
+  "Videographers",
+  "Jewelery",
+  "Hair and Makeup",
+  "Cakes",
+  "Invitations",
+  "Music",
+  "Transportation",
+  "Travel Agents",
+  "Florists",
+  "Resin Artists",
+  "Others"
+];
+
+export default budgetCategories;
